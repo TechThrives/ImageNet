eec0752eb537f8ba1b2e576041d7a17c83baa4ed
NotCoderBoy <robertkexim1998@gmail.com>
2023-12-05 19:05:29 +0530

    first commit

---
+++ pages/user/index.jsx
@@ -0,0 +1,8 @@
+import Layout from "/components/layout";
+import { getCookie } from "cookies-next";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+export default function UserPage() {
+  return <Layout></Layout>;
+}
